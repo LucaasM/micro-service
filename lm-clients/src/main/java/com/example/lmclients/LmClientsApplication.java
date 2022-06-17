084d8c5015209209c612fd9772ca991a08dd00d5
LucaasM <lucas.calixto@dce.ufpb.br>
2022-06-17 19:27:52 -0300

    adding application database configuration

---
+++ lm-clients/src/main/java/com/example/lmclients/LmClientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lmclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmClientsApplication.class, args);
+    }
+
+}
